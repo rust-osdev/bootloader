26e532f45185dbffb6134cb0753cf1da95fc9d70
Philipp Oppermann <dev@phil-opp.com>
2018-01-26 11:58:42 +0100

    Pass boot info to kernel

--- src/context_switch.s
+++ src/context_switch.s
@@ -5,12 +5,15 @@
 # - p4 address in `rdi`
 # - entry point address in `rsi`
 # - stack pointer in `rdx`
+# - boot info ptr in `rcx`
 context_switch:
     # load new P4 table
     mov cr3, rdi
 
     # load stack pointer
     mov rsp, rdx
+
+    mov rdi, rcx
 
     # jump to entry point
     jmp rsi
--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ extern crate os_bootinfo;
 
 pub use x86_64::PhysAddr;
 use x86_64::VirtAddr;
-use x86_64::structures::paging::{PAGE_SIZE, PageTable, PageTableFlags, PhysFrame};
+use x86_64::structures::paging::{PAGE_SIZE, PageTable, PageTableFlags, PhysFrame, Page};
 use core::slice;
 use usize_conversions::{usize_from, FromUsize};
 use os_bootinfo::BootInfo;
@@ -29,7 +29,8 @@ global_asm!(include_str!("memory_map.s"));
 global_asm!(include_str!("context_switch.s"));
 
 extern "C" {
-    fn context_switch(p4_addr: PhysAddr, entry_point: VirtAddr, stack_pointer: VirtAddr) -> !;
+    fn context_switch(p4_addr: PhysAddr, entry_point: VirtAddr, stack_pointer: VirtAddr,
+        boot_info: VirtAddr) -> !;
 }
 
 mod boot_info;
@@ -71,6 +72,11 @@ pub extern "C" fn load_elf(kernel_start: PhysAddr, kernel_size: u64,
 
     let stack_end = page_table::map_kernel(kernel_start, &elf_file, p4, &mut frame_allocator);
 
+    let boot_info_page = Page::containing_address(VirtAddr::new(0xf000000)); // TODO
+    let boot_info_frame = frame_allocator.allocate_frame();
+    let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
+    page_table::map_page(boot_info_page.clone(), boot_info_frame.clone(), flags, p4, &mut frame_allocator);
+
     // identity map VGA text buffer
     let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
     let vga_frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
@@ -78,14 +84,18 @@ pub extern "C" fn load_elf(kernel_start: PhysAddr, kernel_size: u64,
 
     // identity map context switch function to be able to switch P4 tables without page fault
     let context_switch_fn_addr = VirtAddr::new(u64::from_usize(
-        context_switch as *const fn(PhysAddr, VirtAddr, VirtAddr) -> ! as usize));
+        context_switch as *const fn(PhysAddr, VirtAddr, VirtAddr, &'static BootInfo) -> ! as usize));
     let context_switch_fn_frame = PhysFrame::containing_address(
         PhysAddr::new(context_switch_fn_addr.as_u64()));
     let flags = PageTableFlags::PRESENT;
     page_table::identity_map(context_switch_fn_frame, flags, p4, &mut frame_allocator);
 
+    let boot_info_addr = boot_info_page.start_address();
+    let boot_info_ptr = usize_from(boot_info_frame.start_address().as_u64()) as *mut BootInfo;
+    unsafe {boot_info_ptr.write(boot_info)};
+
     let entry_point = VirtAddr::new(elf_file.header.pt2.entry_point());
-    unsafe { context_switch(p4_addr, entry_point, stack_end) };
+    unsafe { context_switch(p4_addr, entry_point, stack_end, boot_info_addr) };
 }
 
 
