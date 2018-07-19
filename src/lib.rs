15f8699a026aae896c062bff2398e0b9ce791c78
Philipp Oppermann <dev@phil-opp.com>
2018-07-19 11:09:43 +0200

    Add an entry_point macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
 #![no_std]
 
 pub extern crate os_bootinfo as bootinfo;
+
+/// Defines the entry point function.
+///
+/// The function must have the signature `fn(&'static BootInfo) -> !`.
+///
+/// This macro just creates a function named `_start`, which the linker will use as the entry
+/// point. The advantage of using this macro instead of providing an own `_start` function is
+/// that the macro ensures that the function and argument types are correct.
+#[macro_export]
+macro_rules! entry_point {
+    ($path:path) => {
+        #[export_name = "_start"]
+        pub extern "C" fn __impl_start(boot_info: &'static $crate::bootinfo::BootInfo) -> ! {
+            // validate the signature of the program entry point
+            let f: fn(&'static $crate::bootinfo::BootInfo) -> ! = $path;
+
+            f(boot_info)
+        }
+    };
+}
