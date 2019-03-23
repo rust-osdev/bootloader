1a17c6322e7e4206a262ad46f6097a3df9744bf3
Philipp Oppermann <dev@phil-opp.com>
2019-03-23 16:19:02 +0100

    Use compile_error to fail compilation for wrong target

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@
 #![no_std]
 #![no_main]
 
+#[cfg(not(target_os = "none"))]
+compile_error!("The bootloader crate must be compiled for the `x86_64-bootloader.json` target");
+
 use bootloader::bootinfo::{BootInfo, FrameRange};
 use core::panic::PanicInfo;
 use core::{mem, slice};
