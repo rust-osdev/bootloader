e2a00a44ff39c74aca712b19b47a69f483938b39
Philipp Oppermann <dev@phil-opp.com>
2019-04-01 13:58:58 +0200

    Add an example kernel that can be used to try the bootloader

---
+++ example-kernel/src/main.rs
@@ -0,0 +1,31 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+
+    let vga_buffer = 0xb8000 as *mut u8;
+    
+    // print `HELLO` to the screen (see
+    // https://os.phil-opp.com/minimal-rust-kernel/#printing-to-screen)
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
