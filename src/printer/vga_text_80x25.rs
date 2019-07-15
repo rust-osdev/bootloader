64c61d87e336041ad64e518ab12a5d508c1deac7
Matt Taylor <mstaveleytaylor@gmail.com>
2019-07-15 18:52:05 +0100

    Use volatile accesses in VGA code and make font dependency optional

--- src/printer/vga_text_80x25.rs
+++ src/printer/vga_text_80x25.rs
@@ -1,35 +1,35 @@
 use core::fmt::{Result, Write};
-use core::slice;
 use core::sync::atomic::{AtomicUsize, Ordering};
 
 const VGA_BUFFER: *mut u8 = 0xb8000 as *mut _;
 const SCREEN_SIZE: usize = 80 * 25;
 
+// must not be 0 so that we don't have a .bss section
 pub static CURRENT_OFFSET: AtomicUsize = AtomicUsize::new(160);
 
 pub struct Printer;
 
 impl Printer {
     pub fn clear_screen(&mut self) {
-        let vga_buffer = Self::vga_buffer();
-        for byte in vga_buffer {
-            *byte = 0;
+        for i in 0..SCREEN_SIZE {
+            unsafe {
+                VGA_BUFFER.offset(i as isize).write_volatile(0);
+            }
         }
-        CURRENT_OFFSET.store(0, Ordering::Relaxed);
-    }
 
-    fn vga_buffer() -> &'static mut [u8] {
-        unsafe { slice::from_raw_parts_mut(VGA_BUFFER, SCREEN_SIZE * 2) }
+        CURRENT_OFFSET.store(0, Ordering::Relaxed);
     }
 }
 
 impl Write for Printer {
     fn write_str(&mut self, s: &str) -> Result {
-        let vga_buffer = Self::vga_buffer();
         for byte in s.bytes() {
-            let index = CURRENT_OFFSET.fetch_add(2, Ordering::Relaxed);
-            vga_buffer[index] = byte;
-            vga_buffer[index + 1] = 0x4f;
+            let index = CURRENT_OFFSET.fetch_add(2, Ordering::Relaxed) as isize;
+
+            unsafe {
+                VGA_BUFFER.offset(index).write_volatile(byte);
+                VGA_BUFFER.offset(index + 1).write_volatile(0x4f);
+            }
         }
 
         Ok(())
