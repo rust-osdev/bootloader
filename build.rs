51a3223fbd264ba580a854a492d7adae85488c98
Matt Taylor <mstaveleytaylor@gmail.com>
2019-08-05 23:44:38 +0100

    Fix attribute on expression error

--- build.rs
+++ build.rs
@@ -60,13 +60,17 @@ fn parse_to_config(cfg: &mut BootloaderConfig, table: &toml::value::Table) {
             }
             ("physical-memory-offset", Value::String(s)) => {
                 #[cfg(feature = "map_physical_memory")]
-                cfg.physical_memory_offset = Some(parse_aligned_addr(key.as_str(), &s));
+                {
+                    cfg.physical_memory_offset = Some(parse_aligned_addr(key.as_str(), &s));
+                }
 
                 #[cfg(not(feature = "map_physical_memory"))]
-                panic!(
-                    "`physical-memory-offset` is only supported when the `map_physical_memory` \
-                     feature of the crate is enabled"
-                );
+                {
+                    panic!(
+                        "`physical-memory-offset` is only supported when the `map_physical_memory` \
+                         feature of the crate is enabled"
+                    );
+                }
             }
             ("kernel-stack-size", Value::Integer(i)) => {
                 if i <= 0 {
