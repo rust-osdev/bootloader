12b3c8b1c7b3ced7f9cd45c7a2df4f6e65066b49
Ethin Probst <harlydavidsen@gmail.com>
2019-09-11 21:21:58 -0500

    Aded bit_field dependency. Formatted code.

--- src/main.rs
+++ src/main.rs
@@ -89,25 +89,26 @@ extern "C" {
 pub unsafe extern "C" fn stage_4() -> ! {
     #[cfg(feature = "sse")]
     {
+        use bit_field::BitField;
         use x86_64::registers::control::Cr0;
-            let mut flags = Cr0::read_raw();
-flags.set_bit(2, false);
-flags.set_bit(1, true);
-flags.set_bit(9, true);
-flags.set_bit(10, true);
-            unsafe {
-Cr0::write_raw(flags);
-}
-            // For now, we must use inline ASM here
-            let mut cr4: u64;
-            unsafe {
-                asm!("mov %cr4, $0" : "=r" (cr4));
-            }
-cr4.set_bit(9, true);
-cr4.set_bit(10, true);
-            unsafe {
-                asm!("mov $0, %cr4" :: "r" (cr4) : "memory");
-            }
+        let mut flags = Cr0::read_raw();
+        flags.set_bit(2, false);
+        flags.set_bit(1, true);
+        flags.set_bit(9, true);
+        flags.set_bit(10, true);
+        unsafe {
+            Cr0::write_raw(flags);
+        }
+        // For now, we must use inline ASM here
+        let mut cr4: u64;
+        unsafe {
+            asm!("mov %cr4, $0" : "=r" (cr4));
+        }
+        cr4.set_bit(9, true);
+        cr4.set_bit(10, true);
+        unsafe {
+            asm!("mov $0, %cr4" :: "r" (cr4) : "memory");
+        }
     }
 
     // Set stack segment
