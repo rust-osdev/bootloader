7352a281f8991a868bcaa4075081d42eb2123ded
Philipp Oppermann <dev@phil-opp.com>
2020-04-15 10:37:24 +0200

    Create a minimal UEFI application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(abi_efiapi)]
+
+use core::panic::PanicInfo;
+use uefi::prelude::{entry, Boot, Handle, Status, SystemTable};
+
+#[entry]
+fn efi_main(image: Handle, st: SystemTable<Boot>) -> Status {
+    Status::SUCCESS
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
