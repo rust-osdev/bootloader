5d4d99ead984380eb75e15b62e1713ca31f2ed4d
Ryland Morgan <ryland.hugo@gmail.com>
2020-04-29 16:48:16 +0100

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,9 +1,11 @@
+// TODO - Make this code less awful
 use llvm_tools::{exe, LlvmTools};
 use std::env;
 use std::path::Path;
 use std::process::Command;
 
 fn main() {
+    println!("cargo:warning=Compiling...");
     let out_dir = env::var("OUT_DIR").unwrap();
     let llvm_tools = LlvmTools::new().expect("LLVM tools not found");
     let objcopy = llvm_tools
@@ -23,6 +25,8 @@ fn main() {
         &objcopy,
     );*/
 
+    println!("cargo:warning=Compiling stage2...");
+
     build_subproject(
         Path::new("src/real/stage_2"),
         &[
@@ -36,14 +40,17 @@ fn main() {
 }
 
 fn build_subproject(
-    dir: &Path,
+    subproject_dir: &Path,
     global_symbols: &[&str],
     target: &str,
     out_dir: &str,
     objcopy: &Path,
 ) {
-    let dir_name = dir.file_name().unwrap().to_str().unwrap();
-    let out_path = Path::new(&out_dir);
+    let dir_name = subproject_dir.file_name().unwrap().to_str().unwrap();
+    let out_dir_path = Path::new(&out_dir);
+
+    let target_dir = out_dir_path.join("..").join("..").join("..").join("..").join("..");
+
     assert!(
         global_symbols.len() > 0,
         "must have at least one global symbol"
@@ -51,25 +58,31 @@ fn build_subproject(
 
     // build
     let mut cmd = Command::new("cargo");
-    cmd.current_dir(&dir);
+    cmd.current_dir(&subproject_dir);
 
     cmd.arg("build").arg("--release").arg("-Zbuild-std=core");    
     cmd.arg("--verbose");
 
     cmd.arg(format!(
         "--target={}",
-        //dir.join(target).display()
         target
     ));
-    cmd.arg("-Z").arg("unstable-options");
+
+    cmd.arg("-Zunstable-options");
     cmd.arg("--out-dir").arg(&out_dir);
     cmd.arg("--target-dir")
-        .arg(out_path.join("target").join(dir_name));
+        .arg(&target_dir);
     cmd.env_remove("RUSTFLAGS");
     cmd.env(
         "XBUILD_SYSROOT_PATH",
-        out_path.join("target").join(dir_name).join("sysroot"),
+        out_dir_path.join("target").join(dir_name).join("sysroot"),
     );
+
+    println!("cargo:warning=Out Dir - {}", &out_dir_path.display());
+    println!("cargo:warning=Subproject Dir - {}", &subproject_dir.display());
+    println!("cargo:warning=Dir Name - {}", &dir_name);
+    println!("cargo:warning=Target Dir - {}", &target_dir.display());
+
     let status = cmd.status().unwrap();
     assert!(status.success(), "Subcrate build failed!");
 
@@ -78,9 +91,9 @@ fn build_subproject(
     for symbol in global_symbols {
         cmd.arg("-G").arg(symbol);
     }
-    cmd.arg(out_path.join(format!("lib{}.a", dir_name)));
+    cmd.arg(target_dir.join(format!("lib{}.a", dir_name)));
     let status = cmd.status().unwrap();
-    assert!(status.success());
+    assert!(status.success(), "Objcopy failed!");
 
     // emit linker flags
     println!("cargo:rustc-link-search=native={}", out_dir);
