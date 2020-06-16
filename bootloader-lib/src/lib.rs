4960b20736650212f2cf5d34f977e840acb8f58b
Philipp Oppermann <dev@phil-opp.com>
2020-06-16 17:25:14 +0200

    Set logging level to minimum (all messages)

--- bootloader-lib/src/lib.rs
+++ bootloader-lib/src/lib.rs
@@ -10,7 +10,7 @@ mod logger;
 pub fn init_logger(framebuffer: &'static mut [u8], info: FrameBufferInfo) {
     let logger = logger::LOGGER.get_or_init(move || logger::LockedLogger::new(framebuffer, info));
     log::set_logger(logger).expect("logger already set");
-    log::set_max_level(log::LevelFilter::Info);
+    log::set_max_level(log::LevelFilter::Trace);
 }
 
 pub fn load_kernel(kernel: &'static [u8]) -> ! {
