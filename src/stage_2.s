83086dc4bb732e4399eb6255ce6ce6f256c5233a
Philipp Oppermann <dev@phil-opp.com>
2021-03-07 17:44:20 +0100

    Replace all remaining `lea`s with `mov` + `offset`

--- src/stage_2.s
+++ src/stage_2.s
@@ -10,13 +10,13 @@ second_stage_start_str: .asciz "Booting (second stage)..."
 kernel_load_failed_str: .asciz "Failed to load kernel from disk"
 
 kernel_load_failed:
-    lea si, [kernel_load_failed_str]
+    mov si, offset kernel_load_failed_str
     call real_mode_println
 kernel_load_failed_spin:
     jmp kernel_load_failed_spin
 
 stage_2:
-    lea si, [second_stage_start_str]
+    mov si, offset second_stage_start_str
     call real_mode_println
 
 set_target_operating_mode:
@@ -33,7 +33,7 @@ set_target_operating_mode:
 
 load_kernel_from_disk:
     # start of memory buffer
-    lea eax, _kernel_buffer
+    mov eax, offset _kernel_buffer
     mov [dap_buffer_addr], ax
 
     # number of disk blocks to load
@@ -50,13 +50,13 @@ load_kernel_from_disk:
     mov edi, 0x400000
 
     # block count
-    lea ecx, _kernel_size
+    mov ecx, offset _kernel_size
     add ecx, 511 # align up
     shr ecx, 9
 
 load_next_kernel_block_from_disk:
     # load block from disk
-    lea si, dap
+    mov si, offset dap
     mov ah, 0x42
     int 0x13
     jc kernel_load_failed
@@ -97,7 +97,7 @@ enter_protected_mode_again:
     mov cr0, eax
 
     push 0x8
-    lea eax, [stage_3]
+    mov eax, offset stage_3
     push eax
     retf
 
