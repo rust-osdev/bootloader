e8fca7b8dd2e54de51cd08cc70ffe8cfb0e6216d
Kenny Strawn <Kenny.Strawn@gmail.com>
2022-01-27 15:11:33 -0800

    `maybe_uninit_extra` is no longer feature-gated in Rust 1.60

--- src/lib.rs
+++ src/lib.rs
@@ -66,7 +66,9 @@ for all possible configuration options.
 */
 
 #![cfg_attr(not(feature = "builder"), no_std)]
-#![feature(maybe_uninit_extra)]
+#![feature(cfg_version)]
+
+#![cfg_attr(not(version("1.60"), feature(maybe_uninit_extra))]
 #![feature(maybe_uninit_slice)]
 #![deny(unsafe_op_in_unsafe_fn)]
 #![warn(missing_docs)]
