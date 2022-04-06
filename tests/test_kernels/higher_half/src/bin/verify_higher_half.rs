af13c11a19d3c94897205068cbaff911e72fd6c9
Tom Dohrmann <erbse.13@gmx.de>
2022-04-06 16:12:55 +0200

    add dynamic range configuration

--- tests/test_kernels/higher_half/src/bin/verify_higher_half.rs
+++ tests/test_kernels/higher_half/src/bin/verify_higher_half.rs
@@ -7,11 +7,25 @@ use test_kernel_higher_half::{exit_qemu, QemuExitCode};
 
 entry_point!(kernel_main);
 
-fn kernel_main(_boot_info: &'static mut BootInfo) -> ! {
+fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
     // verify that kernel is really running in the higher half of the address space
     // (set in `x86_64-higher_half.json` custom target)
     let rip = x86_64::registers::read_rip().as_u64();
     assert_eq!(rip & 0xffffffffffff0000, 0xffff800000000000);
+
+    // verify that the boot info is located in the higher half of the address space
+    assert_eq!(
+        (boot_info as *const _ as usize) & 0xffff800000000000,
+        0xffff800000000000
+    );
+
+    // verify that the stack is located in the higher half of the address space.
+    let stack_addr = &rip;
+    assert_eq!(
+        (boot_info as *const _ as usize) & 0xffff800000000000,
+        0xffff800000000000
+    );
+
     exit_qemu(QemuExitCode::Success);
 }
 
