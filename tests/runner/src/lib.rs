74e173291b0dfcd88babb3f6e26eff8dbb33a1c4
Philipp Oppermann <dev@phil-opp.com>
2022-04-09 20:36:03 +0200

    Fix: Don't pass cli arguments to QEMU

--- tests/runner/src/lib.rs
+++ tests/runner/src/lib.rs
@@ -21,7 +21,6 @@ pub fn run_test_kernel(kernel_binary_path: &str) {
         .arg("-drive")
         .arg(format!("format=raw,file={}", out_gpt_path.display()));
     run_cmd.args(QEMU_ARGS);
-    run_cmd.args(std::env::args().skip(2).collect::<Vec<_>>());
     run_cmd.arg("-bios").arg(ovmf_prebuilt::ovmf_pure_efi());
 
     let child_output = run_cmd.output().unwrap();
