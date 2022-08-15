b69f8a71d9d654a4f1a51d0c043e08f747a4a678
Philipp Oppermann <dev@phil-opp.com>
2022-08-15 11:47:00 +0200

    Add a vga buffer writer to stage 3

--- bios/stage-3/src/main.rs
+++ bios/stage-3/src/main.rs
@@ -2,11 +2,22 @@
 #![no_main]
 #![deny(unsafe_op_in_unsafe_fn)]
 
+use core::fmt::Write as _;
+
+use crate::vga_buffer::Writer;
+
 mod paging;
+mod vga_buffer;
 
 #[no_mangle]
 #[link_section = ".start"]
 pub extern "C" fn _start(stage_4_addr: u32, kernel_addr: u32) {
+    // Writer.clear_screen();
+    writeln!(
+        Writer,
+        "Third Stage (stage_4_addr: {stage_4_addr:#x}, kernel_addr: {kernel_addr:#x})"
+    );
+
     // set up identity mapping, enable paging, and switch CPU into long
     // mode (32-bit compatibility mode)
     paging::init();
@@ -14,16 +25,13 @@ pub extern "C" fn _start(stage_4_addr: u32, kernel_addr: u32) {
     // TODO: Set up long mode with identity-mapping, then jump to 4th stage (passing
     // kernel, memory map, and vesa info as arguments)
 
-    let vga = 0xb8000 as *mut u16;
-
-    for i in 0..(80 * 25) {
-        unsafe { vga.wrapping_add(i).write_volatile(0x0f01) };
-    }
+    writeln!(Writer, "Paging init done");
 
     loop {}
 }
 
 #[panic_handler]
 pub fn panic(info: &core::panic::PanicInfo) -> ! {
+    writeln!(Writer, "PANIC: {info}");
     loop {}
 }
