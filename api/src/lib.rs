f95e9a97190c118f94f914b0c2483b137b8be39d
Philipp Oppermann <dev@phil-opp.com>
2022-09-11 17:55:58 +0200

    Hide items generate by `entry_point` macro from static/fn namespace

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -112,23 +112,25 @@ macro_rules! entry_point {
         $crate::entry_point!($path, config = &$crate::BootloaderConfig::new_default());
     };
     ($path:path, config = $config:expr) => {
-        #[link_section = ".bootloader-config"]
-        pub static __BOOTLOADER_CONFIG: [u8; $crate::BootloaderConfig::SERIALIZED_LEN] = {
-            // validate the type
-            let config: &$crate::BootloaderConfig = $config;
-            config.serialize()
+        const _: () = {
+            #[link_section = ".bootloader-config"]
+            pub static __BOOTLOADER_CONFIG: [u8; $crate::BootloaderConfig::SERIALIZED_LEN] = {
+                // validate the type
+                let config: &$crate::BootloaderConfig = $config;
+                config.serialize()
+            };
+
+            #[export_name = "_start"]
+            pub extern "C" fn __impl_start(boot_info: &'static mut $crate::BootInfo) -> ! {
+                // validate the signature of the program entry point
+                let f: fn(&'static mut $crate::BootInfo) -> ! = $path;
+
+                // ensure that the config is used so that the linker keeps it
+                $crate::__force_use(&__BOOTLOADER_CONFIG);
+
+                f(boot_info)
+            }
         };
-
-        #[export_name = "_start"]
-        pub extern "C" fn __impl_start(boot_info: &'static mut $crate::BootInfo) -> ! {
-            // validate the signature of the program entry point
-            let f: fn(&'static mut $crate::BootInfo) -> ! = $path;
-
-            // ensure that the config is used so that the linker keeps it
-            $crate::__force_use(&__BOOTLOADER_CONFIG);
-
-            f(boot_info)
-        }
     };
 }
 
