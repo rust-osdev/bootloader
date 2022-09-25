9e567ccc47a069dea312df65a8706f82800109c4
Philipp Oppermann <dev@phil-opp.com>
2022-09-25 12:32:16 +0200

    Test booting a kernel compiled with `lto`

---
+++ tests/lto.rs
@@ -0,0 +1,25 @@
+use std::{path::Path, process::Command};
+
+use bootloader_test_runner::run_test_kernel;
+
+#[test]
+fn basic_boot() {
+    // build test kernel manually to force-enable link-time optimization
+    let mut cmd = Command::new(std::env::var("CARGO").unwrap_or("cargo".into()));
+    cmd.arg("build");
+    cmd.arg("-p").arg("test_kernel_lto");
+    cmd.arg("--target").arg("x86_64-unknown-none");
+    cmd.arg("--profile").arg("lto");
+    let status = cmd.status().unwrap();
+    assert!(status.success());
+
+    let root = env!("CARGO_MANIFEST_DIR");
+    let kernel_path = Path::new(root)
+        .join("target")
+        .join("x86_64-unknown-none")
+        .join("lto")
+        .join("basic_boot");
+    assert!(kernel_path.exists());
+
+    run_test_kernel(kernel_path.as_path().to_str().unwrap());
+}
