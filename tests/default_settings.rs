feaa97400aa817c92dbbbc83b24a34712dd43af0
Jason Couture <jasonc@alertr.info>
2023-01-03 19:25:45 -0500

    Convert default_settings to macro

--- tests/default_settings.rs
+++ tests/default_settings.rs
@@ -1,25 +1,9 @@
-use bootloader_test_runner::run_test_kernel;
+use bootloader_test_runner::define_test;
+const BASIC_BOOT_KERNEL: &str = env!("CARGO_BIN_FILE_TEST_KERNEL_DEFAULT_SETTINGS_basic_boot");
+const SHOULD_PANIC_KERNEL: &str = env!("CARGO_BIN_FILE_TEST_KERNEL_DEFAULT_SETTINGS_should_panic");
+const CHECK_BOOT_INFO_KERNEL: &str = env!("CARGO_BIN_FILE_TEST_KERNEL_DEFAULT_SETTINGS_check_boot_info");
 
-#[test]
-fn basic_boot() {
-    run_test_kernel(
-        env!("CARGO_BIN_FILE_TEST_KERNEL_DEFAULT_SETTINGS_basic_boot"),
-        Some("tests/ramdisk.txt"),
-    );
-}
-
-#[test]
-fn should_panic() {
-    run_test_kernel(
-        env!("CARGO_BIN_FILE_TEST_KERNEL_DEFAULT_SETTINGS_should_panic"),
-        None,
-    );
-}
-
-#[test]
-fn check_boot_info() {
-    run_test_kernel(
-        env!("CARGO_BIN_FILE_TEST_KERNEL_DEFAULT_SETTINGS_check_boot_info"),
-        None,
-    );
-}
+define_test!(basic_boot, BASIC_BOOT_KERNEL);
+define_test!(should_panic, SHOULD_PANIC_KERNEL);
+define_test!(check_boot_info, CHECK_BOOT_INFO_KERNEL);
+define_test!(disable_default_ramdisk_macro_test, BASIC_BOOT_KERNEL, without_ramdisk_tests);
