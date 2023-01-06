64faf0faa59535937a047c48c1c6e8ad66b89f41
Jason Couture <jasonc@alertr.info>
2023-01-05 21:16:04 -0500

    Run cargo fmt

--- bios/stage-2/src/main.rs
+++ bios/stage-2/src/main.rs
@@ -101,7 +101,13 @@ fn start(disk_number: u16, partition_table_start: *const u8) -> ! {
     let kernel_page_size = (((kernel_len - 1) / 4096) + 1) as usize;
     let ramdisk_start = KERNEL_DST.wrapping_add(kernel_page_size * 4096);
     writeln!(screen::Writer, "Loading ramdisk...").unwrap();
-    let ramdisk_len = match try_load_file("ramdisk", KERNEL_DST.wrapping_add(kernel_page_size * 4096), &mut fs, &mut disk, disk_buffer) {
+    let ramdisk_len = match try_load_file(
+        "ramdisk",
+        KERNEL_DST.wrapping_add(kernel_page_size * 4096),
+        &mut fs,
+        &mut disk,
+        disk_buffer,
+    ) {
         Some(s) => s,
         None => 0u64,
     };
