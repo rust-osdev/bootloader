0440530f7b480836bdfb3e35e4faba0ae181de70
Tom Dohrmann <erbse.13@gmx.de>
2026-01-11 10:08:00 +0100

    run cargo fmt

--- tests/test_kernels/stack_address/src/bin/basic_boot.rs
+++ tests/test_kernels/stack_address/src/bin/basic_boot.rs
@@ -7,12 +7,14 @@ use test_kernel_stack_address::{BOOTLOADER_CONFIG, QemuExitCode, exit_qemu};
 entry_point!(kernel_main, config = &BOOTLOADER_CONFIG);
 
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
-
     let x: i32 = 42;
     let vaddr = &x as *const _ as usize as u64;
 
     assert_ne!(boot_info.kernel_stack_bottom, 0);
-    assert_eq!(boot_info.kernel_stack_len, BOOTLOADER_CONFIG.kernel_stack_size);
+    assert_eq!(
+        boot_info.kernel_stack_len,
+        BOOTLOADER_CONFIG.kernel_stack_size
+    );
     assert!(vaddr >= boot_info.kernel_stack_bottom);
     assert!(vaddr < boot_info.kernel_stack_bottom + boot_info.kernel_stack_len);
 
